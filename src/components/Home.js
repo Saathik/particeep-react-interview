335a50d07214acadaa136cd3d0e7c61e81d2e47a
Saathik <Saathik@live.fr>
2021-03-26 01:03:43 +0100

    add pagination

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,26 +2,35 @@ import React from "react";
 import { connect } from "react-redux";
 import MovieCard from "./MovieCard";
 import { getMovieList, getMoviesByVisibilityFilter } from "../redux/selectors";
-import { setFilter } from "../redux/actions";
-
+import Pagination from '@material-ui/lab/Pagination';
 import Header from './Header';
 import Footer from './Footer';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const Home = ({ movies }) => {
     return (
         <div>
-            <Header></Header>
-            <div className="main">
+            <Header />
+            <div>
                 < ul className="movie-list" >
                     {movies && movies.length
                         ? movies.map((movie) => {
                             return <MovieCard key={`movie-${movie.id}`} movie={movie} />;
                         })
-                        : "No movies, yay!"
+                        : "No more movies!"
                     }
                 </ul >
+                <div className="pagination">
+                    <Pagination count={3} color="primary" />
+                    <Select value="12">
+                        <MenuItem value={4}>4</MenuItem>
+                        <MenuItem value={8}>8</MenuItem>
+                        <MenuItem value={12}>12</MenuItem>
+                    </Select>
+                </div>
             </div>
-            <Footer></Footer>
+            <Footer />
         </div>
     );
 }
@@ -35,4 +44,5 @@ const mapStateToProps = state => {
 
     return { movies, options };
 }
-export default connect(mapStateToProps, { setFilter })(Home);
+export default connect(mapStateToProps)(Home);
+
